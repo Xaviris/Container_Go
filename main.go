e44af5b0e93cd86b46f82a2aa65fbd3a6853ee65
root <root@pop-os.localdomain>
2023-10-22 10:06:35 -0500

    chroot | mount proc | mount namespace

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
+	fmt.Printf("Running %v\n as %d\n", os.Args[2:], os.Getpid())
 
 	// set command to be executed from arguments
 	// re invoke process with command child args...
@@ -36,8 +36,10 @@ func run() {
 
 	// create new namespace
 	// UTS = Unix Time System
+	// name space for mount to avoid cluttering host mount
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
 	}
 
 	// run set command inside new namespace
@@ -49,11 +51,17 @@ func run() {
 // and can't create it after because it has exited the Run command
 // re invoking the process inside the namespace will modify hostname before exiting Run
 func child() {
-	fmt.Printf("Running Child %v\n", os.Args[2:])
+	fmt.Printf("Running %v\n as %d\n", os.Args[2:], os.Getpid())
 
 	// set namespace hostname
 	syscall.Sethostname([]byte("container"))
 
+	// change root directory to new ubuntu fs
+	syscall.Chroot("/home/xavierruiz/Documents/Github/Container_Go/ubuntu-chroot")
+	syscall.Chdir("/")
+
+	// mount proc directory as sub file system for kernel
+	syscall.Mount("proc", "proc", "proc", 0, "")
 	// set command to be executed from arguments
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 
@@ -64,6 +72,9 @@ func child() {
 
 	// run set command
 	cmd.Run()
+
+	// unmount /proc when finished
+	syscall.Unmount("/proc", 0)
 }
 
 func must(err error) {
---
+++ ubuntu-chroot/root/.bash_history
@@ -0,0 +1,11 @@
+ps
+ls
+cd proc
+clea
+clear
+ls
+clear
+ps
+exit
+ps
+exit
